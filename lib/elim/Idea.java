e1b320b5149e90bebce3180dfdf8cc9a0c3800f0
ABdeeglr <abdeeglr@icloud.com>
2023-06-11 20:03:35 +0800

    添加了 Idea 类, 我需要了解 java 中如何实例化新的对象

---
+++ lib/elim/Idea.java
@@ -0,0 +1,17 @@
+package lib.elim;
+
+/**
+ * 被捕获的想法
+ */
+public class Idea {
+    private String message; // 额外信息
+    private String name; // 名称
+    private int id; // 唯一 ID
+    private int father_id; // 父想法的 ID
+    private int locateTimeChunk; // 所在的时间区块
+
+    
+    public Idea(String name) {
+
+    }
+}
